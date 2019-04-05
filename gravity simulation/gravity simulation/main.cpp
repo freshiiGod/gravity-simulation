b3f575e4090374f2c9ce3845d2373fda0175f2aa
freshiiGod <tylerchapmqn@gmail.com>
2019-04-04 23:58:06 -0400

    update main.cpp

--- gravity simulation/gravity simulation/main.cpp
+++ gravity simulation/gravity simulation/main.cpp
@@ -1,4 +1,29 @@
+#include <SFML/Graphics.hpp>
+
 int main()
 {
+	sf::RenderWindow window(sf::VideoMode(600, 600), "Gravity", sf::Style::Close);
+
+	while (window.isOpen)
+	{
+		sf::Event event;
+		while (window.pollEvent(event))
+		{
+			if (event.type == event.Closed)
+			{
+				window.close();
+				break;
+			}
+		}
+		//clear window with black color
+		window.clear(sf::Color::Black);
+
+		//select what to draw: window.draw(...)
+
+
+		//actually draw objects
+		window.display();
+	}
+
 	return 0;
 }
